563acde27214bea687560e8debfba12091f4746f
Priyanshu Kumar <priyanshukumar55a@gmail.com>
2026-05-14 22:31:06 +0530

    fixed

--- utils/mailer.js
+++ utils/mailer.js
@@ -12,7 +12,9 @@ const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASS
-    }
+    },
+    logger: true,
+    debud: true
 });
 
 if (process.env.NODE_ENV !== "production") {
