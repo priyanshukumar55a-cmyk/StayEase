c25c3415b54eda1e9c85394d434619f4d6e1718a
Priyanshu Kumar <priyanshukumar55a@gmail.com>
2026-05-16 23:00:23 +0530

    fixed upload section

--- routes/hostRouter.js
+++ routes/hostRouter.js
@@ -1,15 +1,24 @@
 // External Module
 const express = require("express");
 const hostRouter = express.Router();
+const upload = require("../middleware/multer");
 
 // Local Module
 const hostController = require("../controllers/hostController");
 
 hostRouter.get("/add-home", hostController.getAddHome);
-hostRouter.post("/add-home", hostController.postAddHome);
+hostRouter.post(
+    "/add-home",
+    upload.single("photo"),
+    hostController.postAddHome
+);
 hostRouter.get("/host-home-list", hostController.getHostHomes);
 hostRouter.get("/edit-home/:homeId", hostController.getEditHome);
-hostRouter.post("/edit-home", hostController.postEditHome);
+hostRouter.post(
+    "/edit-home",
+    upload.single("photo"),
+    hostController.postEditHome
+);
 hostRouter.post("/delete-home/:homeId", hostController.postDeleteHome);
 
 module.exports = hostRouter;
