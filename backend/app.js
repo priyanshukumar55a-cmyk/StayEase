c31c4c5550f8ac344286175f3cd0697b3f49b339
Priyanshu Kumar <priyanshukumar55a@gmail.com>
2026-07-19 21:58:29 +0530

    Update backend configuration

--- backend/app.js
+++ backend/app.js
@@ -31,7 +31,7 @@ const cors = require("cors");
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", ""],
     credentials: true,
   }),
 );
